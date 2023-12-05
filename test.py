80c982fd1a7f3fa73ad2e5104e59a7bd57e040a9
n_k <pekolian-dengi@yandex.ru>
2023-12-05 22:57:56 +0300

    commit_test

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+# test_задачки
 def f():
     return(23, 34, 67)
 print(f())
@@ -7,3 +8,12 @@ def f1():
        yield i
 s = f1()
 print(s)
+# test_задачка с факториалом
+def fact(n):
+    pr =1
+    a = []
+    for i in range(1, n+1):
+        pr =pr * i
+        a.append(pr)
+    return a
+print(fact(10))
