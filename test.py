b396d3b390ff6bff9d2dcbcda1089a2e61d2419a
n_k <pekolian-dengi@yandex.ru>
2023-12-06 17:13:59 +0300

    test_commit

--- test.py
+++ test.py
@@ -1,30 +1,30 @@
-#                            # test_Z
-# def f():
-#     return(23, 34, 67)
-# print(f())
-# print(f())
-# def f1():
-#     for i in (23 ,34, 67):
-#        yield i
-# s = f1()
-# print(s)
-#            # test_Z        С ФАКТОРИАЛОМ
-# def fact(n):
-#     pr =1
-#     a = []
-#     for i in range(1, n+1):
-#         pr =pr * i
-#         a.append(pr)
-#     return a
-# print(fact(10))
-#  # test_задачка таже что с верху но с помощью генератора.
-# def fact(n):
-#     pr = 1
-#     for i in range(1, n+1):
-#         pr = pr*i
-#         yield pr
-# for i in fact(10):
-#     print(i, end=", ")
+                           # test_Z
+def f():
+    return(23, 34, 67)
+print(f())
+print(f())
+def f1():
+    for i in (23 ,34, 67):
+       yield i
+s = f1()
+print(s)
+           # test_Z        С ФАКТОРИАЛОМ
+def fact(n):
+    pr =1
+    a = []
+    for i in range(1, n+1):
+        pr =pr * i
+        a.append(pr)
+    return a
+print(fact(10))
+     # test_задачка таже что с верху но с помощью генератора.
+def fact(n):
+    pr = 1
+    for i in range(1, n+1):
+        pr = pr*i
+        yield pr
+for i in fact(10):
+    print(i, end=", ")
 
 
 
